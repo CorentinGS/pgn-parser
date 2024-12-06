5178c7ccef13d98e71dddacb70d14b01bc3f7044
CorentinGS <contact@corentings.dev>
2024-12-06 19:09:54 +0100

    refactor: 🔥 dead code
    Signed-off-by: CorentinGS <contact@corentings.dev>

--- utils.go
+++ utils.go
@@ -8,13 +8,6 @@ func isDigit(ch byte) bool {
 	return '0' <= ch && ch <= '9'
 }
 
-func isSquare(s string) bool {
-	if len(s) != 2 {
-		return false
-	}
-	return 'a' <= s[0] && s[0] <= 'h' && '1' <= s[1] && s[1] <= '8'
-}
-
 func isWhitespace(ch byte) bool {
 	return ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r'
 }
@@ -26,8 +19,4 @@ func isResult(s string) bool {
 // Helper function to check if a character is a valid file
 func isFile(ch byte) bool {
 	return ch >= 'a' && ch <= 'h'
-}
-
-func isRow(ch byte) bool {
-	return ch >= '1' && ch <= '8'
 }
